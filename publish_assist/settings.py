bd666c214df0968defef980fa0e3239e7f241996
sarveshwar08 <sarveshwar.pandey0805@gmail.com>
2026-01-30 04:25:12 +0000

    feat: finalize readme and cleanup

--- publish_assist/settings.py
+++ publish_assist/settings.py
@@ -8,24 +8,12 @@ from pydantic import Field
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    # --- Required settings even when working locally. ---
-
     # Groq
     GROQ_API_KEY: str | None = None
     
     #WEBSHARE
     WEBSHARE_USERNAME: str | None = None
     WEBSHARE_PASSWORD: str | None = None
-
-    # Huggingface API
-    HUGGINGFACE_ACCESS_TOKEN: str | None = None
-
-    # Comet ML (during training)
-    COMET_API_KEY: str | None = None
-    COMET_PROJECT: str = "twin"
-
-    # --- Required settings when deploying the code. ---
-    # --- Otherwise, default values values work fine. ---
 
     # MongoDB database
     DATABASE_HOST: str = "mongodb://admin:password@localhost:27017"
@@ -38,53 +26,11 @@ class Settings(BaseSettings):
     USE_QDRANT_CLOUD: bool = False
     QDRANT_DATABASE_HOST: str | None = None
     QDRANT_DATABASE_PORT: str | None = None
-
-    # AWS Authentication
-    AWS_REGION: str = "eu-central-1"
-    AWS_ACCESS_KEY: str | None = None
-    AWS_SECRET_KEY: str | None = None
-    AWS_ARN_ROLE: str | None = None
-
-    # --- Optional settings used to tweak the code. ---
-
-    # AWS SageMaker
-    HF_MODEL_ID: str = "mlabonne/TwinLlama-3.1-8B-DPO"
-    GPU_INSTANCE_TYPE: str = "ml.g5.2xlarge"
-    SM_NUM_GPUS: int = 1
-    MAX_INPUT_LENGTH: int = 2048
-    MAX_TOTAL_TOKENS: int = 4096
-    MAX_BATCH_TOTAL_TOKENS: int = 4096
-    COPIES: int = 1  # Number of replicas
-    GPUS: int = 1  # Number of GPUs
-    CPUS: int = 2  # Number of CPU cores
-
-    SAGEMAKER_ENDPOINT_CONFIG_INFERENCE: str = "twin"
-    SAGEMAKER_ENDPOINT_INFERENCE: str = "twin"
-    TEMPERATURE_INFERENCE: float = 0.01
-    TOP_P_INFERENCE: float = 0.9
-    MAX_NEW_TOKENS_INFERENCE: int = 150
-
+    
     # RAG
     TEXT_EMBEDDING_MODEL_ID: str = "sentence-transformers/all-MiniLM-L6-v2"
     RERANKING_CROSS_ENCODER_MODEL_ID: str = "cross-encoder/ms-marco-MiniLM-L-4-v2"
     RAG_MODEL_DEVICE: str = "cpu"
-
-    # LinkedIn Credentials
-    LINKEDIN_USERNAME: str | None = None
-    LINKEDIN_PASSWORD: str | None = None
-
-    @property
-    def OPENAI_MAX_TOKEN_WINDOW(self) -> int:
-        official_max_token_window = {
-            "gpt-3.5-turbo": 16385,
-            "gpt-4-turbo": 128000,
-            "gpt-4o": 128000,
-            "gpt-4o-mini": 128000,
-        }.get(self.OPENAI_MODEL_ID, 128000)
-
-        max_token_window = int(official_max_token_window * 0.90)
-
-        return max_token_window
 
     @classmethod
     def load_settings(cls) -> "Settings":
